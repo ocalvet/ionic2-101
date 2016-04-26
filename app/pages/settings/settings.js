b5f1b358b03ab8f2c991678bfe1609a06130c8e5
Ovidio R. Calvet <calveto@gmail.com>
2016-04-25 23:55:59 -0400

    Added the add child functionality to the settings page

--- app/pages/settings/settings.js
+++ app/pages/settings/settings.js
@@ -1,10 +1,26 @@
-import {Page} from 'ionic-angular';
+import {Page, NavController, Modal} from 'ionic-angular';
+import {AddChildModalPage} from '../../modals/addChildModal/addChildModalPage';
 
 @Page({
   templateUrl: 'build/pages/settings/index.html'
 })
 export class SettingsPage {
-  constructor() {
+  static get parameters() {
+    return [[NavController]];
+  }
 
+  constructor(nav) {
+    this.nav = nav;
+  }
+
+  addChild() {
+    let addModal = Modal.create(AddChildModalPage);
+    addModal.onDismiss((data) => {
+      if (data) {
+        console.log('new child', data);
+        // this.children.push(data);
+      }
+    });
+    this.nav.present(addModal);
   }
 }
