3936672812935edff1d910b9866f6a7f380d90d2
Ovidio R. Calvet <calveto@gmail.com>
2016-04-28 22:14:29 -0400

    placed the settings into an object within the service that gets updated from the settings page making the change happen in the child list page

--- app/services/settingsService.js
+++ app/services/settingsService.js
@@ -1,12 +1,13 @@
 export class SettingsService {
   constructor() {
+    this.settings = {
+      showTasks: true
+    };
   }
 
   getSettings() {
     return new Promise((resolve, reject) => {
-      resolve({
-        showTasks: true
-      });
+      resolve(this.settings);
     });
   }
 }
